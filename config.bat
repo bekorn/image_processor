ff16d296b3a9a6cb0e78fa78e302380334766cc0
bekorn <cbaenrlki@gmail.com>
2024-01-07 19:07:47 +0300

    fixes OMP dll issue again

--- config.bat
+++ config.bat
@@ -3,3 +3,7 @@
 pushd "%VS2022INSTALLDIR%"
 call "VC\Auxiliary\Build\vcvars64.bat"
 popd
+
+rem https://stackoverflow.com/questions/34439956/vc-crash-when-freeing-a-dll-built-with-openmp
+rem This has to be set before a dll is run, setting it in run.bat doesn't affect vscode debugging
+set OMP_WAIT_POLICY=passive
--- run.bat
+++ run.bat
@@ -1,7 +1,1 @@
-@echo off
-
-rem https://stackoverflow.com/questions/34439956/vc-crash-when-freeing-a-dll-built-with-openmp
-rem This has to be set before the dll is run, here works well
-set OMP_WAIT_POLICY=passive
-
 build\main.exe MrIncredible.png
